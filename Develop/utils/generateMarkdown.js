3b6629ad8e0e5f25e61161e617ca432bade77204
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-02-26 19:03:04 +0000

    completing functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,11 +13,23 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license != "None") {
+    return `- [License](#license)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != "None") {
+    return `This project uses the ${license} license.`;
+  } else {
+    return "No license is used for this project";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -30,7 +42,7 @@ function generateMarkdown(data) {
   - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
-  - [License](#license)
+  ${renderLicenseLink(data.license)}
   - [Tests](#tests)
   - [Contributing](#contributing)
   - [Questions](#questions)
@@ -57,9 +69,10 @@ function generateMarkdown(data) {
   * Email - ${data.email}
 
   ## License
-  This project is licensed under 
-
+  ${renderLicenseSection(data.license)}
+  
   ${renderLicenseBadge(data.license)}
+
 `;
 }
 
