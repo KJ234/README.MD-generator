7450600183cb0fbf9d7bf4fcdf39e0ba58eef095
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-02-26 14:15:25 +0000

    adding the first question

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,21 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Project title?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter a Project Title");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
