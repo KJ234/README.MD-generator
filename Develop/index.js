568fa2ef71d32d03d9b4594af28eb2bb3c79a132
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-02-26 15:42:28 +0000

    completing questions

--- Develop/index.js
+++ Develop/index.js
@@ -19,6 +19,78 @@ const questions = [
       }
     },
   },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your Project title?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter a Project Title");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Add a description for your project?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please provide a description for your project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter installation instructions");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this app?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter a usage description!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "GNU"],
+    default: ["MIT"],
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please choose a license!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "What does the user need to know about contributing to the repo?",
+  },
 ];
 
 // TODO: Create a function to write README file
